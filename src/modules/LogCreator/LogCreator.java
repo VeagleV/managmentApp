d58720a17745c5f409ac1ee3da539b068a81b23b
Eagle <70978893+VeagleV@users.noreply.github.com>
2024-12-17 02:03:05 +0300

    lil refactor changes

--- src/modules/LogCreator/LogCreator.java
+++ src/modules/LogCreator/LogCreator.java
@@ -8,10 +8,13 @@ import java.util.logging.Logger;
 public class LogCreator {
     //данный класс предназначен для инициализация логгера, и его конфигурирования через log.config
     public static Logger LOGGER;
+
     static {
         try(FileInputStream ins = new FileInputStream("src/modules/LogCreator/log.config")){ //относительный путь до файла с конфигами
             LogManager.getLogManager().readConfiguration(ins);
             LOGGER = Logger.getLogger(LogCreator.class.getName());
+
+
         }catch (Exception ignore){
             ignore.printStackTrace();
         }
--- src/modules/Tasks/Timed/TimedTask.java
+++ src/modules/Tasks/Timed/TimedTask.java
@@ -14,6 +14,7 @@ public class TimedTask extends Task {
 
     private LocalDateTime plannedTime;
     private boolean isExpired = false;
+
     public TimedTask(){
         super();
     }
